27601ba24582c273b2f3dd0e3376ff2ba2a679ab
Paul Lesieur <plesieur@jpkeefehs.org>
2021-09-25 19:46:19 -0400

    Add project files.

---
+++ Residence/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Residence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Class!");
+            Console.WriteLine("Don't forget to add test code here as well");
+        }
+    }
+}
